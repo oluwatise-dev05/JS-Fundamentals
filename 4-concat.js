69c92c42564a9d78b02b664e6c2db3f0af5bf123
oluwatise-dev05 <oluwatiseadetolaa@gmail.com>
2025-05-25 15:57:32 +0100

    Update 4-concat.js
    ok

--- 4-concat.js
+++ 4-concat.js
@@ -1,16 +1,4 @@
-const args = ['c', 'cool'];
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
 
-const arg1 = args[0];
-const arg2 = args[1];
-console.log(`${arg1 || 'undefined'} is ${arg2 || 'undefined'}`);
-
-
-const args2 = ['c'];
-const arg1_2 = args2[0];
-const arg2_2 = args2[1];
-console.log(`${arg1_2 || 'undefined'} is ${arg2_2 || 'undefined'}`);
-
-const args3 = [];
-const arg1_3 = args3[0];
-const arg2_3 = args3[1];
-console.log(`${arg1_3 || 'undefined'} is ${arg2_3 || 'undefined'}`);
+console.log(`${arg1} is ${arg2}`);
