241df654d7251b274da3fdf9e5c9e5331e67f5c9
oluwatise-dev05 <oluwatiseadetolaa@gmail.com>
2025-05-25 13:48:56 +0100

    Update 4-concat.js
    we go again take 4

--- 4-concat.js
+++ 4-concat.js
@@ -6,3 +6,7 @@ if (arg1 !== undefined && arg2 !== undefined) {
 } else if (arg1 !== undefined) {
   console.log("HBTN");
 }
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+console.log(`${arg1 === undefined ? 'undefined' : arg1} is ${arg2 === undefined ? 'undefined' : arg2}`);
